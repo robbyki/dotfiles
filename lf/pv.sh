dbbad5295b7d5c9482a00bcea4862c5bcdabeb59
rkee <rkee@us.ibm.com>
2021-10-31 09:28:23 -0400

    adding lf files

---
+++ lf/pv.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+unset COLORTERM
+
+filename="$(basename "$1")"
+EXT="${filename##*.}"
+
+# lowercasing
+# if this doesn't work check if you bash is old like 3.x on like on macOS
+# then `$ brew install bash`
+ext="${EXT,,}"
+
+if [ "${ext}" == "md" ]; then
+  echo '`# Markdown: '"$1"'`via **glow**' | glow - -s dark
+  /usr/local/bin/glow "$1"
+else
+  bat --color=always --theme=base16 "$@"
+fi
