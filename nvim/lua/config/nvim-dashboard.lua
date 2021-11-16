581e39b1a739afdd902740c00d1c46e1b4547062
rkee <rkee@us.ibm.com>
2021-11-16 17:10:46 -0500

    fixing dashboard location settings

--- nvim/lua/config/nvim-dashboard.lua
+++ nvim/lua/config/nvim-dashboard.lua
@@ -21,7 +21,7 @@ vim.g.dashboard_custom_section = {
     c = {description = {'  New File            '}, command = ':DashboardNewFile'},
     d = {description = {'  Projects            '}, command = 'Telescope project'},
     e = {description = {'  Config              '}, command = ':cd ~/.config/nvim | :Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍'},
-    f = {description = {'  Git Projects        '}, command = ':cd ~/dev/git-projects | :Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍'},
+    f = {description = {'  IBM Projects        '}, command = ':cd ~/dev/ibm-projects | :Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍'},
     g = {description = {'  Aide Template       '}, command = ':cd ~/dev/git-projects/aide-template | :Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍'}
     -- e = {description = {'  Marks              '}, command = 'Telescope marks'}
 }
