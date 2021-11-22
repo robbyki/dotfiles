ea3f088afffe9380a80ce60515b7e14ead13e589
rkee <rkee@us.ibm.com>
2021-11-22 13:49:12 -0500

    ""

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,136 +1,136 @@
-local M = {}
-
-M.setup = function()
-  local shared_diagnostic_settings = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
-  local lsp_config = require("lspconfig")
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-  lsp_config.util.default_config = vim.tbl_extend("force", lsp_config.util.default_config, {
-    handlers = {
-      ["textDocument/publishDiagnostics"] = shared_diagnostic_settings,
-    },
-    capabilities = capabilities,
-  })
-
-  Metals_config = require("metals").bare_config
-  Metals_config.settings = {
-    showImplicitArguments = true,
-    showInferredType = true,
-    excludedPackages = {
-      "akka.actor.typed.javadsl",
-      "com.github.swagger.akka.javadsl",
-      "akka.stream.javadsl",
-    },
-    --fallbackScalaVersion = "2.13.5"
-  }
-
-  Metals_config.init_options.statusBarProvider = "on"
-  Metals_config.handlers["textDocument/publishDiagnostics"] = shared_diagnostic_settings
-  Metals_config.capabilities = capabilities
-
-  local dap = require("dap")
-
-  -- For that they usually provide a `console` option in their |dap-configuration|.
-  -- The supported values are usually called `internalConsole`, `integratedTerminal`
-  -- and `externalTerminal`.
-
-  dap.configurations.scala = {
-    {
-      type = "scala",
-      request = "launch",
-      name = "Run",
-      metalsRunType = "run",
-    },
-    {
-      type = "scala",
-      request = "launch",
-      name = "Test File",
-      metalsRunType = "testFile",
-    },
-    {
-      type = "scala",
-      request = "launch",
-      name = "Test Target",
-      metalsRunType = "testTarget",
-    },
-  }
-
-  Metals_config.on_attach = function(client, bufnr)
-    require("metals").setup_dap()
-  end
-
---  require'lspconfig'.pyright.setup{on_attach=require'compe'.on_attach}
-
-  -- sumneko lua
-  lsp_config.sumneko_lua.setup({
-    cmd = {
-      "/home/robbyk/tools/lua-language-server/bin/Linux/lua-language-server",
-      "-E",
-      "/home/robbyk/tools/lua-language-server/main.lua",
-    },
-    commands = {
-      Format = {
-        function()
-          require("stylua-nvim").format_file()
-        end,
-      },
-    },
-    settings = {
-      Lua = {
-        runtime = {
-          version = "LuaJIT", -- since using mainly for neovim
-          path = vim.split(package.path, ";"),
-        },
-        diagnostics = { globals = { "vim", "it" } },
-        workspace = {
-          checkThirdParty = false,
-          preloadFileSize = 10000,
-          maxPreload = 10000,
-          -- Make the server aware of Neovim runtime files
-          library = {
-            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-          },
-        },
-      },
-    },
-  })
-
-  lsp_config.bashls.setup{ on_attach=on_attach }
-  lsp_config.pyright.setup{ on_attach=on_attach }
-  lsp_config.dockerls.setup({})
-  lsp_config.html.setup({})
-  lsp_config.jsonls.setup({
-    commands = {
-      Format = {
-        function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
-        end,
-      },
-    },
-  })
-  lsp_config.tsserver.setup{ on_attach=on_attach }
-  lsp_config.yamlls.setup{ on_attach=on_attach }
-  lsp_config.racket_langserver.setup{ on_attach=on_attach }
-
-  lsp_config.gopls.setup({
-    cmd = { "gopls", "serve" },
-    settings = {
-      gopls = { analyses = { unusedparams = true }, staticcheck = true },
-    },
-  })
-
-  --lsp_config.groovyls.setup({
-  --  cmd = {
-  --    "java",
-  --    "-jar",
-  --    "/Users/ckipp/Documents/groovy-workspace/groovy-language-server/build/libs/groovy-language-server-all.jar",
-  --  },
-  --})
-
-  -- Uncomment for trace logs from neovim
-  --vim.lsp.set_log_level('trace')
-end
-
-return M
+--local M = {}
+--
+--M.setup = function()
+--  local shared_diagnostic_settings = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+--  local lsp_config = require("lspconfig")
+--  local capabilities = vim.lsp.protocol.make_client_capabilities()
+--  capabilities.textDocument.completion.completionItem.snippetSupport = true
+--
+--  lsp_config.util.default_config = vim.tbl_extend("force", lsp_config.util.default_config, {
+--    handlers = {
+--      ["textDocument/publishDiagnostics"] = shared_diagnostic_settings,
+--    },
+--    capabilities = capabilities,
+--  })
+--
+--  Metals_config = require("metals").bare_config
+--  Metals_config.settings = {
+--    showImplicitArguments = true,
+--    showInferredType = true,
+--    excludedPackages = {
+--      "akka.actor.typed.javadsl",
+--      "com.github.swagger.akka.javadsl",
+--      "akka.stream.javadsl",
+--    },
+--    --fallbackScalaVersion = "2.13.5"
+--  }
+--
+--  Metals_config.init_options.statusBarProvider = "on"
+--  Metals_config.handlers["textDocument/publishDiagnostics"] = shared_diagnostic_settings
+--  Metals_config.capabilities = capabilities
+--
+--  local dap = require("dap")
+--
+--  -- For that they usually provide a `console` option in their |dap-configuration|.
+--  -- The supported values are usually called `internalConsole`, `integratedTerminal`
+--  -- and `externalTerminal`.
+--
+--  dap.configurations.scala = {
+--    {
+--      type = "scala",
+--      request = "launch",
+--      name = "Run",
+--      metalsRunType = "run",
+--    },
+--    {
+--      type = "scala",
+--      request = "launch",
+--      name = "Test File",
+--      metalsRunType = "testFile",
+--    },
+--    {
+--      type = "scala",
+--      request = "launch",
+--      name = "Test Target",
+--      metalsRunType = "testTarget",
+--    },
+--  }
+--
+--  Metals_config.on_attach = function(client, bufnr)
+--    require("metals").setup_dap()
+--  end
+--
+----  require'lspconfig'.pyright.setup{on_attach=require'compe'.on_attach}
+--
+--  -- sumneko lua
+--  lsp_config.sumneko_lua.setup({
+--    cmd = {
+--      "/home/robbyk/tools/lua-language-server/bin/Linux/lua-language-server",
+--      "-E",
+--      "/home/robbyk/tools/lua-language-server/main.lua",
+--    },
+--    commands = {
+--      Format = {
+--        function()
+--          require("stylua-nvim").format_file()
+--        end,
+--      },
+--    },
+--    settings = {
+--      Lua = {
+--        runtime = {
+--          version = "LuaJIT", -- since using mainly for neovim
+--          path = vim.split(package.path, ";"),
+--        },
+--        diagnostics = { globals = { "vim", "it" } },
+--        workspace = {
+--          checkThirdParty = false,
+--          preloadFileSize = 10000,
+--          maxPreload = 10000,
+--          -- Make the server aware of Neovim runtime files
+--          library = {
+--            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+--            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+--          },
+--        },
+--      },
+--    },
+--  })
+--
+--  lsp_config.bashls.setup{ on_attach=on_attach }
+--  lsp_config.pyright.setup{ on_attach=on_attach }
+--  lsp_config.dockerls.setup({})
+--  lsp_config.html.setup({})
+--  lsp_config.jsonls.setup({
+--    commands = {
+--      Format = {
+--        function()
+--          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+--        end,
+--      },
+--    },
+--  })
+--  lsp_config.tsserver.setup{ on_attach=on_attach }
+--  lsp_config.yamlls.setup{ on_attach=on_attach }
+--  lsp_config.racket_langserver.setup{ on_attach=on_attach }
+--
+--  lsp_config.gopls.setup({
+--    cmd = { "gopls", "serve" },
+--    settings = {
+--      gopls = { analyses = { unusedparams = true }, staticcheck = true },
+--    },
+--  })
+--
+--  --lsp_config.groovyls.setup({
+--  --  cmd = {
+--  --    "java",
+--  --    "-jar",
+--  --    "/Users/ckipp/Documents/groovy-workspace/groovy-language-server/build/libs/groovy-language-server-all.jar",
+--  --  },
+--  --})
+--
+--  -- Uncomment for trace logs from neovim
+--  --vim.lsp.set_log_level('trace')
+--end
+--
+--return M
--- nvim/lua/config/nvim-autopairs.lua
+++ nvim/lua/config/nvim-autopairs.lua
@@ -76,19 +76,19 @@
 --imap("<Tab>", "v:lua.MUtils.tab()", {expr = true, noremap = true})
 --imap("<S-Tab>", "v:lua.MUtils.s_tab()", {expr = true, noremap = true})
 
-local M = {}
-
-function M.config()
-	require('nvim-autopairs').setup {
-		disable_filetype = { "TelescopePrompt", "vim" },
-		check_ts = true,
-		enable_check_bracket_line = true,
-		map_cr = true,
-		map_complete = true,
-  }
-  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-  local cmp = require('cmp')
-  cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-end
-
-return M
+--local M = {}
+--
+--function M.config()
+--	require('nvim-autopairs').setup {
+--		disable_filetype = { "TelescopePrompt", "vim" },
+--		check_ts = true,
+--		enable_check_bracket_line = true,
+--		map_cr = true,
+--		map_complete = true,
+--  }
+--  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+--  local cmp = require('cmp')
+--  cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+--end
+--
+--return M
