cbb6f2c47ca3e4d64b27bad50f60f49262e5f94d
rkee <rkee@us.ibm.com>
2021-12-05 06:13:35 -0500

    updating telescope config and formatting

--- nvim/lua/config/nvim-telescope.lua
+++ nvim/lua/config/nvim-telescope.lua
@@ -1,140 +1,175 @@
-local telescope = require("telescope")
-local telescope_actions = require("telescope.actions")
+local telescope = require "telescope"
+local telescope_actions = require "telescope.actions"
 -- local actions = require('telescope.actions')
 
-require('telescope').setup {
-    defaults = {
-        vimgrep_arguments = {
-		"rg",
-	        "--with-filename",
-		"--no-heading",
-		"--line-number",
-		"--column",
-		"--hidden",
-		"--smart-case",
-		"--follow",
-		"--color=never",
-        },
-        prompt_prefix = "❯ ",
-        -- prompt_prefix = " ",
-        selection_caret = " ",
-        entry_prefix = "  ",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        layout_config = {
-            horizontal = {
-                prompt_position = "top",
-                preview_width = 0.55,
-                results_width = 0.8,
-            },
-            vertical = { mirror = false },
-            width = 0.87,
-            height = 0.80,
-            preview_cutoff = 120,
-        },
-        -- layout_config = {horizontal = {mirror = false}, vertical = {mirror = false}},
-        file_sorter = require("telescope.sorters").get_fzy_sorter,
-        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-        file_ignore_patterns = {
-          "Downloads",
-          ".bloop",
-          ".metals",
-          ".git",
-          "cache",
-          "target",
-          "node_modules",
-          "parser.c",
-          "ScalaResources",
-          "lua-language-server"
-        },
-        path_diplay = { "shorten" },
-        winblend = 0,
-        border = {},
-        -- borderchars = { '═', '│', '═', '│', '╒', '╕', '╛', '╘' },
-        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-        color_devicons = true,
-        use_less = true,
-        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
-        mappings = {
-            i = {
-                ["<C-j>"] = telescope_actions.move_selection_next,
-                ["<C-k>"] = telescope_actions.move_selection_previous,
-                ["<Esc>"] = telescope_actions.close,
-                ["<Tab>"] = telescope_actions.toggle_selection + telescope_actions.move_selection_next,
-                ["<S-Tab>"] = telescope_actions.toggle_selection + telescope_actions.move_selection_previous,
-                -- ["<CR>"] = actions.select_default + actions.center
-            },
-            n = {
-                ["<C-j>"] = telescope_actions.move_selection_next,
-                ["<C-k>"] = telescope_actions.move_selection_previous,
-                ["<C-q>"] = telescope_actions.smart_send_to_qflist + telescope_actions.open_qflist,
-            }
-        },
+require("telescope").setup {
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--with-filename",
+      "--no-heading",
+      "--line-number",
+      "--column",
+      "--hidden",
+      "--smart-case",
+      "--follow",
+      "--color=never",
     },
-    extensions = {
-        fzf = {
-           fuzzy = true, -- false will only do exact matching
-           override_generic_sorter = false, -- override the generic sorter
-           override_file_sorter = true, -- override the file sorter
-           case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-        },
-        media_files = {
-           filetypes = { "png", "webp", "jpg", "jpeg" },
-           find_cmd = "rg",
-        },
-        -- apparently this would control where telescope opens the preview
-        project = {
-          -- base_dirs = {'~/dev'},
-          hidden_files = true,
-          display_type = "full"
-        }
+    dynamic_preview_title = true,
+    prompt_prefix = "❯ ",
+    -- prompt_prefix = " ",
+    selection_caret = " ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    -- layout_config = {
+    --   horizontal = {
+    --     preview_width = 0.6,
+    --   },
+    -- },
+    layout_strategy = "flex",
+    layout_config = {
+      -- confusing but horizontal means side by side
+      horizontal = { width = 0.9, height = 0.95, preview_width = 0.6 },
+      vertical = { width = 0.8, height = 0.8, preview_height = 0.7 },
+      -- horizontal = {
+      --   prompt_position = "bottom",
+      --   preview_width = 0.55,
+      -- },
+      -- vertical = {
+      --   mirror = false,
+      -- },
+      -- width = 0.87,
+      -- height = 0.80,
+      -- preview_cutoff = 120,
     },
+
+    -- layout_strategy = "horizontal",
+    -- layout_config = {
+    --     horizontal = {
+    --         prompt_position = "top",
+    --         preview_width = 0.55,
+    --         results_width = 0.8,
+    --     },
+    --     vertical = { mirror = false },
+    --     width = 0.87,
+    --     height = 0.80,
+    --     preview_cutoff = 120,
+    -- },
+    -- layout_config = {horizontal = {mirror = false}, vertical = {mirror = false}},
+    file_sorter = require("telescope.sorters").get_fzy_sorter,
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+    file_ignore_patterns = {
+      "Downloads",
+      ".bloop",
+      ".metals",
+      ".git",
+      "cache",
+      "target",
+      "node_modules",
+      "parser.c",
+      "ScalaResources",
+      "lua-language-server",
+      "db",
+      "gif",
+      "jpeg",
+      "jpg",
+      "ods",
+      "odt",
+      "pdf",
+      "png",
+      "svg",
+      "xcf",
+      "xls",
+    },
+    path_display = { "shorten" },
+    winblend = 0,
+    border = {},
+    -- borderchars = { '═', '│', '═', '│', '╒', '╕', '╛', '╘' },
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    use_less = true,
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    mappings = {
+      i = {
+        ["<C-j>"] = telescope_actions.move_selection_next,
+        ["<C-k>"] = telescope_actions.move_selection_previous,
+        ["<Esc>"] = telescope_actions.close,
+        ["<Tab>"] = telescope_actions.toggle_selection + telescope_actions.move_selection_next,
+        ["<S-Tab>"] = telescope_actions.toggle_selection + telescope_actions.move_selection_previous,
+        -- ["<CR>"] = actions.select_default + actions.center
+      },
+      n = {
+        ["<C-j>"] = telescope_actions.move_selection_next,
+        ["<C-k>"] = telescope_actions.move_selection_previous,
+        ["<C-q>"] = telescope_actions.smart_send_to_qflist + telescope_actions.open_qflist,
+      },
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+    },
+    media_files = {
+      filetypes = { "png", "webp", "jpg", "jpeg" },
+      find_cmd = "rg",
+    },
+    -- apparently this would control where telescope opens the preview
+    project = {
+      -- base_dirs = {'~/dev'},
+      hidden_files = true,
+      display_type = "full",
+    },
+  },
 }
-telescope.load_extension('fzf')
-telescope.load_extension('media_files')
-telescope.load_extension('project')
-telescope.load_extension("zoxide")
-telescope.load_extension("neoclip")
+telescope.load_extension "fzf"
+telescope.load_extension "media_files"
+telescope.load_extension "project"
+telescope.load_extension "zoxide"
+telescope.load_extension "neoclip"
 
-require("neoclip").setup({
-	default_register = { "+", "*" },
-	filter = nil,
-	history = 1000,
-})
+require("neoclip").setup {
+  default_register = { "+", "*" },
+  filter = nil,
+  history = 1000,
+}
 
-local previewers = require('telescope.previewers')
-local Job = require('plenary.job')
+local previewers = require "telescope.previewers"
+local Job = require "plenary.job"
 local new_maker = function(filepath, bufnr, opts)
   filepath = vim.fn.expand(filepath)
-  Job:new({
-    command = 'file',
-    args = { '--mime-type', '-b', filepath },
-    on_exit = function(j)
-      local mime_type = vim.split(j:result()[1], '/')[1]
-      if mime_type == "text" then
-        previewers.buffer_previewer_maker(filepath, bufnr, opts)
-      else
-        -- maybe we want to write something to the buffer here
-        vim.schedule(function()
-          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'BINARY' })
-        end)
-      end
-    end
-  }):sync()
+  Job
+    :new({
+      command = "file",
+      args = { "--mime-type", "-b", filepath },
+      on_exit = function(j)
+        local mime_type = vim.split(j:result()[1], "/")[1]
+        if mime_type == "text" then
+          previewers.buffer_previewer_maker(filepath, bufnr, opts)
+        else
+          -- maybe we want to write something to the buffer here
+          vim.schedule(function()
+            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+          end)
+        end
+      end,
+    })
+    :sync()
 end
 
-require('telescope').setup {
+require("telescope").setup {
   defaults = {
     buffer_previewer_maker = new_maker,
-  }
+  },
 }
 -- require('telescope').load_extension('fzy_native')
-
 
 --local telescope_map = function(key, provider)
 --    utils.map("n", "<leader>f" .. key, "<CMD>lua require('telescope.builtin')." .. provider .. "()<CR>")
@@ -183,7 +218,6 @@ require('telescope').setup {
 --    }
 --  }
 --}
-
 
 -- https://github.com/nvim-telescope/telescope.nvim
 --local M = {}
