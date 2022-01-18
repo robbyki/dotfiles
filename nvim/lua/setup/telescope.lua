137ce604a27986155148b737bdc62950115aed9c
Robby Kiskanyan <rkee@us.ibm.com>
2022-01-18 05:25:35 -0500

    telescope cleanup

--- nvim/lua/setup/telescope.lua
+++ nvim/lua/setup/telescope.lua
@@ -1,7 +1,19 @@
 local telescope = require("telescope")
 local telescope_actions = require("telescope.actions")
 
-require("telescope").setup({
+local toggle_modes = function()
+  local mode = vim.api.nvim_get_mode().mode
+  if mode == "n" then
+    vim.cmd [[startinsert]]
+    return
+  end
+  if mode == "i" then
+    vim.cmd [[stopinsert]]
+    return
+  end
+end
+
+telescope.setup({
     defaults = {
         vimgrep_arguments = {
             "rg",
@@ -63,75 +75,71 @@ require("telescope").setup({
         qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
         buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         file_ignore_patterns = {
-            "Downloads",
+            "%.gif",
+            "%.jpeg",
+            "%.jpg",
+            "%.ods",
+            "%.odt",
+            "%.oh%-my%-zsh/",
+            "%.pdf",
+            "%.png",
+            "%.svg",
+            "%.xcf",
+            "%.xls",
+            "%.zcompdump",
             ".bloop",
-            ".metals",
+            ".dropbox%-dist/",
+            ".dropbox%-dist/",
             ".git/*",
+            ".jfrog/",
+            ".m2",
+            ".metals",
             ".metals/*",
-            "cache",
-            "target",
-            "node_modules",
-            "parser.c",
+            ".mozilla/",
+            ".vscode/",
+            ".zoom/",
+            "Downloads",
             "ScalaResources",
-            "lua-language-server",
+            "ScalaResources/",
+            "^lua-language-server/",
+            "cache",
             "db",
             "gif",
             "jpeg",
             "jpg",
+            "lua-language-server",
+            "node_modules",
             "ods",
             "odt",
+            "parser.c",
             "pdf",
             "png",
             "svg",
+            "target",
             "xcf",
-            "xls",
-            "ScalaResources/",
-            "^lua-language-server/",
-            ".dropbox%-dist/",
-            ".vscode/",
-            ".dropbox%-dist/",
-            -- ".mozilla/",
-            -- ".zoom/",
-            -- ".jfrog/",
+            "xls"
             -- ".bluemix/",
             -- "db",
             -- "%.vim/",
             -- "%.local/",
             -- "%.cache/",
-            -- "%.oh%-my%-zsh/",
-            ".m2",
-            -- "%.gif",
-            "%.zcompdump",
-            -- - "%.jpeg",
-            -- - "%.jpg",
-            -- - "%.ods",
-            -- - "%.odt",
-            -- - "%.pdf",
-            -- - "%.png",
-            -- - "%.svg",
-            -- - "%.xcf",
-            -- - "%.xls"
         },
         path_display = { "shorten" },
         winblend = 0,
         border = {},
-        -- borderchars = { '═', '│', '═', '│', '╒', '╕', '╛', '╘' },
-        -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
         color_devicons = true,
         use_less = true,
         set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
         mappings = {
             i = {
-                -- ["<C-S-j>"] = telescope_actions.move_selection_next,
-                -- ["<C-S-k>"] = telescope_actions.move_selection_previous,
-                -- ["<Esc>"] = telescope_actions.close,
+                ["<C-space>"] = toggle_modes,
                 ["<Tab>"] = telescope_actions.toggle_selection + telescope_actions.move_selection_next,
                 ["<S-Tab>"] = telescope_actions.toggle_selection + telescope_actions.move_selection_previous,
+                ["<Esc>"] = telescope_actions.close
                 -- ["<CR>"] = actions.select_default + actions.center
             },
             n = {
-                -- ["<C-S-j>"] = telescope_actions.move_selection_next,
-                -- ["<C-S-k>"] = telescope_actions.move_selection_previous,
+                ["<C-space>"] = toggle_modes,
                 ["<C-q>"] = telescope_actions.smart_send_to_qflist + telescope_actions.open_qflist,
             },
         },
@@ -161,209 +169,3 @@ telescope.load_extension("project")
 telescope.load_extension("zoxide")
 telescope.load_extension("file_browser")
 -- telescope.load_extension("neoclip")
-
--- local previewers = require "telescope.previewers"
--- local Job = require "plenary.job"
--- local new_maker = function(filepath, bufnr, opts)
---   filepath = vim.fn.expand(filepath)
---   Job
---     :new({
---       command = "file",
---       args = { "--mime-type", "-b", filepath },
---       on_exit = function(j)
---         local mime_type = vim.split(j:result()[1], "/")[1]
---         if mime_type == "text" then
---           previewers.buffer_previewer_maker(filepath, bufnr, opts)
---         else
---           -- maybe we want to write something to the buffer here
---           vim.schedule(function()
---             vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
---           end)
---         end
---       end,
---     })
---     :sync()
--- end
-
--- require("telescope").setup {
---   defaults = {
---     buffer_previewer_maker = new_maker,
---   },
--- }
--- require('telescope').load_extension('fzy_native')
-
---local telescope_map = function(key, provider)
---    utils.map("n", "<leader>f" .. key, "<Cmd>lua require('telescope.builtin')." .. provider .. "()<CR>")
---end
---
---telescope_map("b", "buffers")
---telescope_map("d", "lsp_document_diagnostics")
---telescope_map("D", "lsp_workspace_diagnostics")
---telescope_map(":", "command_history")
---telescope_map("f", "find_files")
---telescope_map("g", "git_files")
---telescope_map("h", "help_tags")
---telescope_map("p", "projects")
---telescope_map("r", "live_grep")
---telescope_map("R", "live_grep grep_open_files=true")
---telescope_map("/", "search_history")
---telescope_map("s", "lsp_document_symbols")
---telescope_map("T", "current_buffer_tags")
---telescope_map("t", "tags")
---
---utils.map("n", "<leader>fp", "<Cmd>lua require('telescope').extensions.projects.projects()<CR>")
-
---require'telescope'.setup {
---    extensions = {
---        media_files = {
---            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
---            filetypes = {"png", "webp", "jpg", "jpeg"},
---            find_cmd = "rg" -- find command (defaults to `fd`)
---        },
---        fzy_native = {
---            override_generic_sorter = false,
---            override_file_sorter = true,
---        },
---    project = {
---      display_type = 'full'
---    }
---  }
---}
---require('telescope').setup {
---  extensions = {
---    project = {
---      base_dirs = {
---        {'~/dev'}
---      },
---      hidden_files = true -- default: false
---    }
---  }
---}
-
--- https://github.com/nvim-telescope/telescope.nvim
---local M = {}
---
---M.setup = function()
---  require("telescope").setup({
---    defaults = {
---      file_ignore_patterns = { "target", "node_modules", "parser.c", "ScalaResources" },
---      prompt_prefix = "❯",
---      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
---      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
---    },
---  })
---
---  require("telescope").load_extension("fzy_native")
---end
---
----- This is mainly for Metals since we don't respond to "" as a query to get all
----- the symbols. This will first get the input form the user and then execute
----- the query.
---M.lsp_workspace_symbols = function()
---  local input = vim.fn.input("Query: ")
---  vim.api.nvim_command("normal :esc<CR>")
---  if not input or #input == 0 then
---    return
---  end
---  require("telescope.builtin").lsp_workspace_symbols({ query = input })
---end
---
---return M
---
---
--- Telescope Global remapping
---local action_state = require("telescope.actions.state")
---local actions = require("telescope.actions")
---local fb_actions = require("telescope._extensions.file_browser.actions")
---
---require("telescope").setup({
---  defaults = {
---    winblend = 20,
---    sorting_strategy = "descending",
---    layout_strategy = "flex",
---    layout_config = {
---      flex = {
---        flip_columns = 140,
---      },
---      vertical = {
---        preview_cutoff = 40,
---        prompt_position = "bottom",
---      },
---      horizontal = {
---        width = 0.9,
---        height = 0.8,
---      },
---    },
---    mappings = {
---      i = {
---        ["<esc>"] = actions.close,
---        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
---      },
---    },
---  },
---  extensions = {
---    file_browser = {
---      theme = "ivy",
---      mappings = {
---        ["i"] = {
---          ["<S-M>"] = fb_actions.move_file,
---        },
---        ["n"] = {
---          -- your custom normal mode mappings
---        },
---      },
---    },
---  },
---  pickers = {
---    buffers = {
---      sort_lastused = true,
---      mappings = {
---        i = {
---          ["<C-w>"] = "delete_buffer",
---        },
---        n = {
---          ["<C-w>"] = "delete_buffer",
---        },
---      },
---    },
---    -- https://gitter.im/nvim-telescope/community?at=6113b874025d436054c468e6 Fabian David Schmidt
---    find_files = {
---      on_input_filter_cb = function(prompt)
---        local find_colon = string.find(prompt, ":")
---        if find_colon then
---          local ret = string.sub(prompt, 1, find_colon - 1)
---          vim.schedule(function()
---            local prompt_bufnr = vim.api.nvim_get_current_buf()
---            local picker = action_state.get_current_picker(prompt_bufnr)
---            local lnum = tonumber(prompt:sub(find_colon + 1))
---            if type(lnum) == "number" then
---              local win = picker.previewer.state.winid
---              local bufnr = picker.previewer.state.bufnr
---              local line_count = vim.api.nvim_buf_line_count(bufnr)
---              vim.api.nvim_win_set_cursor(win, { math.max(1, math.min(lnum, line_count)), 0 })
---            end
---          end)
---          return { prompt = ret }
---        end
---      end,
---      attach_mappings = function()
---        actions.select_default:enhance({
---          post = function()
---            -- if we found something, go to line
---            local prompt = action_state.get_current_line()
---            local find_colon = string.find(prompt, ":")
---            if find_colon then
---              local lnum = tonumber(prompt:sub(find_colon + 1))
---              vim.api.nvim_win_set_cursor(0, { lnum, 0 })
---            end
---          end,
---        })
---        return true
---      end,
---    },
---  },
---})
---
---require("telescope").load_extension("fzf")
---require("telescope").load_extension("sessions")
---require("telescope").load_extension("file_browser")
