2bf5a380a7bea3125b1da91ef06c1f2328c5f776
Robby Kiskanyan <robbyk@li-25ea9c4c-20a1-11b2-a85c-b853287c8397.ibm.com>
2022-04-07 18:38:05 -0400

    adding scripts folder

---
+++ scripts/config-links.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+ln -s -f ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s -f ~/.dotfiles/bat/config ~/.config/bat/config
+ln -s -f ~/.dotfiles/broot/conf.hjson ~/.config/broot/conf.hjson
+ln -s -f ~/.dotfiles/fzf/key-bindings.zsh ~/.fzf/shell/key-bindings.zsh
+ln -s -f ~/.dotfiles/gh/config.yml ~/.config/gh/config.yml
+ln -s -f ~/.dotfiles/lf ~/.config/lf
+ln -s -f ~/.dotfiles/zsh/alias.zsh ~/.oh-my-zsh/custom/alias.zsh
+ln -s -f ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s -f ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+ln -s -f ~/.dotfiles/zsh/functions.zsh ~/.oh-my-zsh/custom/functions.zsh
+ln -s -f ~/.dotfiles/zsh/fzf-tab.zsh ~/.oh-my-zsh/custom/fzf-tab.zsh
+ln -s -f ~/.dotfiles/zsh/completions ~/.oh-my-zsh/completions
