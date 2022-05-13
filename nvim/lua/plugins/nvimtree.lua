673dfd0b5e70c204832a079657274acb6b7d748b
Robby Kiskanyan <robbmk@gmail.com>
2022-05-13 15:56:02 -0400

    updating nvim-tree settings

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,29 +1,48 @@
-vim.g.nvim_tree_respect_buf_cwd = 1
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+}
 
 require("nvim-tree").setup({
   disable_netrw = true,
   hijack_netrw = true,
-  update_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
     },
   },
   filters = {
     dotfiles = true,
     custom = {},
   },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
   git = {
     enable = true,
-    ignore = true,
-    timeout = 400,
+    ignore = false,
+    timeout = 500,
   },
 })
